3ab7eb9d3bc834cf6a606bd4967851e89de78871
Amit Mehta <amit.mehta@intelligentpathways.com.au>
2017-10-25 20:54:11 +1100

    removed unwanted imports and code

--- src/main/java/com/example/sample/crm/samplecrm/SwaggerConfiguration.java
+++ src/main/java/com/example/sample/crm/samplecrm/SwaggerConfiguration.java
@@ -2,7 +2,6 @@ package com.example.sample.crm.samplecrm;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
